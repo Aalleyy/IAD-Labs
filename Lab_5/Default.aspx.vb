deb3f107a46dafdbf045794f4684f17d03a3be9d
Ali Raza <142317127+Aalleyy@users.noreply.github.com>
2025-04-10 14:07:18 +0500

    Updated Default.aspx.vb for Server

--- Lab_5/Default.aspx.vb
+++ Lab_5/Default.aspx.vb
@@ -1,36 +1,40 @@
-﻿Imports System.Data.SqlClient
-Imports System.Security.Policy
+Imports System.Data.SqlClient
 
 Partial Class _Default
     Inherits System.Web.UI.Page
 
-    Private Sub _Default_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim constr As String
-        constr = "Data Source= localhost; Initial Catalog= ArtVault; User ID=ALI; Password= 12345"
-        Dim con As New SqlConnection
-        con.ConnectionString = constr
-        Dim cmd As New SqlCommand
-        cmd.Connection = con
-        cmd.CommandText = "INSERT INTO CUSTOMER_T (NAME, EMAIL, PHONE, CNIC)"
-        cmd.CommandText &= " VALUES ('"
-        cmd.CommandText &= name.Value & "', '"
-        cmd.CommandText &= email.Value & "', '"
-        cmd.CommandText &= phone.Value & "', '"
-        cmd.CommandText &= cnic.Value & "')"
-        Try
-            con.Open()
-            Dim insert As Integer = cmd.ExecuteNonQuery()
-            If insert > 0 Then
-                Response.Write("Record Inserted Successfully")
-            Else
-                Response.Write("Record Insertion Failed")
-            End If
-        Catch ex As Exception
-            Response.Write(ex.Message)
-        Finally
-            con.Close()
-            cmd.Dispose()
-        End Try
+    Protected Sub add_ServerClick(ByVal sender As Object, ByVal e As EventArgs)
+        Dim constr As String = "workstation id=ArtVault.mssql.somee.com;packet size=4096;user id=Aalleyy_SQLLogin_1;pwd=6hqepy6m5t;data source=ArtVault.mssql.somee.com;persist security info=False;initial catalog=ArtVault;TrustServerCertificate=True"
+        Using con As New SqlConnection(constr)
+            Dim query As String = "INSERT INTO PERSON (PERSON_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NO, GENDER, CITY, STATE, POSTAL_CODE, COUNTRY, ROLE, PASSWORD) " &
+                                  "VALUES (@person_id, @fname, @lname, @email, @phone, @gender, @city, @state, @postal, @country, @role, @password)"
 
+            Using cmd As New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@person_id", cnic.Value)
+                cmd.Parameters.AddWithValue("@fname", fname.Value)
+                cmd.Parameters.AddWithValue("@lname", lname.Value)
+                cmd.Parameters.AddWithValue("@email", email.Value)
+                cmd.Parameters.AddWithValue("@phone", phone.Value)
+                cmd.Parameters.AddWithValue("@gender", gender.Value)
+                cmd.Parameters.AddWithValue("@city", city.Value)
+                cmd.Parameters.AddWithValue("@state", state.Value)
+                cmd.Parameters.AddWithValue("@postal", postal.Value)
+                cmd.Parameters.AddWithValue("@country", country.Value)
+                cmd.Parameters.AddWithValue("@role", role.Value)
+                cmd.Parameters.AddWithValue("@password", password.Value)
+
+                Try
+                    con.Open()
+                    Dim result As Integer = cmd.ExecuteNonQuery()
+                    If result > 0 Then
+                        Response.Write("Record Inserted Successfully!")
+                    Else
+                        Response.Write("Insertion Failed.")
+                    End If
+                Catch ex As Exception
+                    Response.Write("Error: " & ex.Message)
+                End Try
+            End Using
+        End Using
     End Sub
 End Class
