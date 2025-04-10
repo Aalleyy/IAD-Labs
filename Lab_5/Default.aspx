46e8232b25d17edd5a2f77b5739d2f73b0d9f947
Ali Raza <142317127+Aalleyy@users.noreply.github.com>
2025-04-10 13:21:45 +0500

    Add files via upload

---
+++ Lab_5/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Registration Form</h1>
+            <label for="name">Name:</label>
+            <input id="name" type="text" name="name" value="" runat="server"/></br>
+            <label for="email">email:</label>
+            <input id="email" type="text" name="email" value="" runat="server" /></br>
+            <label for="phone">phone:</label>
+            <input id="phone" type="text" name="phone" value="" runat="server"/></br>
+            <label for="cnic">cnic:</label>
+            <input id="cnic" type="text" name="phone" value="" runat="server"/></br>
+
+            <input id="add" type="submit" value="Submit" runat="server"/></br>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab_5/Default.aspx.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+Imports System.Security.Policy
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Private Sub _Default_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim constr As String
+        constr = "Data Source= localhost; Initial Catalog= ArtVault; User ID=ALI; Password= 12345"
+        Dim con As New SqlConnection
+        con.ConnectionString = constr
+        Dim cmd As New SqlCommand
+        cmd.Connection = con
+        cmd.CommandText = "INSERT INTO CUSTOMER_T (NAME, EMAIL, PHONE, CNIC)"
+        cmd.CommandText &= " VALUES ('"
+        cmd.CommandText &= name.Value & "', '"
+        cmd.CommandText &= email.Value & "', '"
+        cmd.CommandText &= phone.Value & "', '"
+        cmd.CommandText &= cnic.Value & "')"
+        Try
+            con.Open()
+            Dim insert As Integer = cmd.ExecuteNonQuery()
+            If insert > 0 Then
+                Response.Write("Record Inserted Successfully")
+            Else
+                Response.Write("Record Insertion Failed")
+            End If
+        Catch ex As Exception
+            Response.Write(ex.Message)
+        Finally
+            con.Close()
+            cmd.Dispose()
+        End Try
+
+    End Sub
+End Class
